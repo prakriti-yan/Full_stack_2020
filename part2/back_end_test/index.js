0338dc602e4d41c53a0eca7318e84fd91babb7ba
Yan Yan <yyan@c2r2p13.hive.fi>
2020-01-31 14:08:03 +0200

    back_end_test project

---
+++ part2/back_end_test/index.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+
+let notes = [
+	{
+	  id: 1,
+	  content: "HTML is easy",
+	  date: "2019-05-30T17:30:31.098Z",
+	  important: true
+	},
+	{
+	  id: 2,
+	  content: "Browser can execute only Javascript",
+	  date: "2019-05-30T18:39:34.091Z",
+	  important: false
+	},
+	{
+	  id: 3,
+	  content: "GET and POST are the most important methods of HTTP protocol",
+	  date: "2019-05-30T19:20:14.298Z",
+	  important: true
+	}
+  ]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello Hivers!</h1>')
+})
+
+app.get('/notes', (req, res) => {
+  res.json(notes)
+})
+
+app.get('/notes/:id', (request, response) => {
+	const id = Number(request.params.id)
+	const note = notes.find(note=>note.id === id)
+	// response.json(note)
+	if (note){
+		response.json(note)
+	}else{
+		response.status(404).end()
+	}
+})
+
+
+app.delete('/notes/:id', (request, response) => {
+	const id = Number(request.params.id)
+	notes = notes.filter(note => note.id !== id)
+  
+	response.status(204).end()
+  })
+
+  app.post('/notes', (request, response) => {
+	const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id)) 
+    : 0
+
+  const note = request.body
+  note.id = maxId + 1
+
+  notes = notes.concat(note)
+
+  response.json(note)
+  })
+
+const PORT = 3002
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
